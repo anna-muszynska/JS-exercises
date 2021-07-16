54c6eb27aabf55cf231ef168d715afb34d9eea04
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-07-16 20:52:03 +0200

    Values, variables, data types

--- 01 Fundamentals/assignments.js
+++ 01 Fundamentals/assignments.js
@@ -1,6 +1,16 @@
-//1
+/*
+// 1st
 let country = "Poland";
 let continent = "Europe";
 let population = 38000000;
 
 console.log(country, continent, population);
+
+// 2nd
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- 01 Fundamentals/script.js
+++ 01 Fundamentals/script.js
@@ -1,3 +1,5 @@
+/*
+// Values and Variables
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -10,6 +12,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variable name conventions
 let jonas_matilda = "JM";
 let $function = 27;
 
@@ -22,3 +25,27 @@ let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);
+*/
+
+/*
+// Data types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
