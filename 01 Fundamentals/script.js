b693e90b11c1f01e317b4a9a2fbdaa7fa881d461
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-07-19 20:49:25 +0200

    Truthy and falsy values

--- 01 Fundamentals/script.js
+++ 01 Fundamentals/script.js
@@ -182,3 +182,27 @@ let n = '1' + 1;
 n = n - 1;
 console.log(n);
 */
+
+/*
+// Truthy and falsy values
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Anna')); // truthy
+console.log(Boolean({})); // truthy
+console.log(Boolean(''));
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log('You should get a job!');
+}
+
+let height;
+if (height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED');
+}
+*/
