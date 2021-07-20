d9a45e142f7fad2d6530cfe714ea22a623aa62a4
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-07-20 19:40:10 +0200

    The switch statement

--- 01 Fundamentals/assignments.js
+++ 01 Fundamentals/assignments.js
@@ -153,4 +153,29 @@ if (scoreAvgDolphins >= 100 && scoreAvgDolphins > scoreAvgKoalas) {
 } else {
     console.log(`Nobody won the game :(`);
 }
+
+
+// 9th
+let language = 'chinese';
+
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too! :D');
+}
 */
--- 01 Fundamentals/script.js
+++ 01 Fundamentals/script.js
@@ -253,3 +253,31 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
     console.log('Someone else should drive...');
 }
 */
+
+/*
+// The switch statement
+const day = 'monday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to cinema');
+        break;
+    case 'tuesday':
+        console.log('Meet mom at cafe');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Take Marcel to the dog park');
+        break;
+    case 'friday':
+        console.log('Practice programming skills');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend!');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+*/
