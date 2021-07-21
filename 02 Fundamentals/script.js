8291e37dc142b17a87d6f58393bad47203e30b67
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-07-21 18:46:07 +0200

    Functions

---
+++ 02 Fundamentals/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*
+// Functions
+function logger() {
+    console.log('My name is Ania');
+}
+
+// Calling, running of invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(4, 4);
+console.log(appleOrangeJuice);
+*/
