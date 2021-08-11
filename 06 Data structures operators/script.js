a5f7d88cca3423ba43daac122edff83e8ceaac25
Anna <ania.maria.muszynska@gmail.com>
2021-08-11 18:18:55 +0200

    Short circuiting (|| and &&)

--- 06 Data structures operators/script.js
+++ 06 Data structures operators/script.js
@@ -51,8 +51,41 @@ const restaurant = {
 };
 
 ///////////////////////////////////////////////
-// Rest pattern and parameters
+// Short circuiting (|| and &&)
 
+// Logical operators can: 1) use ANY data type, 2) return ANY data type, 3) short-circuiting
+
+console.log('----------OR----------');
+console.log(3 || 'Anna');
+console.log('' || 'Anna');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 23;
+
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('----------AND----------');
+console.log(0 && 'Anna');
+console.log(7 && 'Anna');
+
+console.log('Hello' && 23 && null && 'Anna');
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+///////////////////////////////////////////////
+// Rest pattern and parameters
+/*
 // 1) Destructuring
 
 // SPREAD, because on RIGHT side of =
@@ -88,6 +121,7 @@ add(...x);
 
 restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
 restaurant.orderPizza('mushrooms');
+*/
 
 ///////////////////////////////////////////////
 // The spread operator (...)
