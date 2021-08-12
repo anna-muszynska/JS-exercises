7050ec16c3741aae063cbc0110c57b9008455fa9
Anna <ania.maria.muszynska@gmail.com>
2021-08-12 21:48:40 +0200

    Looping arrays: the for-of loop

--- 06 Data structures operators/script.js
+++ 06 Data structures operators/script.js
@@ -51,8 +51,109 @@ const restaurant = {
 };
 
 ///////////////////////////////////////////////
-// The nullish coalescing operator (??)
+// Looping arrays: the for-of loop
 
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+// console.log([...menu.entries()]);
+
+///////////////////////////////////////////////
+// Coding Challenge #1
+/*
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1
+// const players1 = game.players[0];
+// const players2 = game.players[1];
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4
+const players1Final = [...players1, 'Thiago', 'Couthino', 'Perisic'];
+console.log(players1Final);
+
+// 5
+// const { team1, x: draw, team2 } = game.odds;
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// 6
+function printGoals(...names) {
+  //   let goals = 0;
+  //   for (let i = 0; i < names.length; i++) {
+  //     console.log(names[i]);
+  //     goals++;
+  //   }
+  //   console.log('Goals:', goals);
+
+  console.log(`${names.length} goals were score`);
+}
+
+printGoals(...game.scored);
+
+// 7
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+*/
+
+///////////////////////////////////////////////
+// The nullish coalescing operator (??)
+/*
 restaurant.numGuests = 0;
 const guests2 = restaurant.numGuests || 10;
 console.log(guests2);
@@ -60,6 +161,7 @@ console.log(guests2);
 // Nullish: null and undefined (NOT 0 or '')
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
+*/
 
 ///////////////////////////////////////////////
 // Short circuiting (|| and &&)
