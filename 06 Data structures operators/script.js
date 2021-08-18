832a3c9b80ddfe7c9a4acf6fb20735199b88b394
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-08-18 20:15:23 +0200

    Working with strings - part 2

--- 06 Data structures operators/script.js
+++ 06 Data structures operators/script.js
@@ -55,8 +55,70 @@ const restaurant = {
 };
 
 ///////////////////////////////////////////////
-// Working with strings - part 1
+// Working with strings - part 2
 
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'aNiA'; // Ania
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Comparing email
+const email = 'hello@ania.io';
+const loginEmail = '   Hello@Ania.Io\n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// Replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+// console.log(announcement.replaceAll('door', 'gate'));
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+///////////////////////////////////////////////
+// Working with strings - part 1
+/*
 const airline = 'TAP Air Portugal';
 const plane = 'A320';
 
@@ -95,6 +157,7 @@ checkMiddleSeat('3E');
 
 console.log(new String('anna'));
 console.log(typeof new String('anna'));
+*/
 
 ///////////////////////////////////////////////
 // Challenge #3
