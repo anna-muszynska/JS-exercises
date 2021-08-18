6cc9bbfd72028b488eee76b8cc2a5dce62f81a94
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-08-18 22:38:54 +0200

    String methods practice

--- 06 Data structures operators/script.js
+++ 06 Data structures operators/script.js
@@ -55,6 +55,28 @@ const restaurant = {
 };
 
 ///////////////////////////////////////////////
+// String methods practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+console.log(flights.split('+'));
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(44);
+  console.log(output);
+}
+
+///////////////////////////////////////////////
 // Challenge #4
 /*
 const input = document.createElement('textarea');
@@ -84,7 +106,7 @@ button.addEventListener('click', functionInput);
 
 ///////////////////////////////////////////////
 // Working with strings - part 3
-
+/*
 console.log('a+very+nice+string'.split('+'));
 console.log('Anna Muszyńska'.split(' '));
 
@@ -133,6 +155,7 @@ const planesInLine = function (n) {
 planesInLine(5);
 planesInLine(3);
 planesInLine(12);
+*/
 
 ///////////////////////////////////////////////
 // Working with strings - part 2
