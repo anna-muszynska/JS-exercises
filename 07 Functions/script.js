68a1f62b5dca4c466754ae5c7a934506e6e2b0e0
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-08-20 21:48:38 +0200

    How passing arguments works: value vs. reference

--- 07 Functions/script.js
+++ 07 Functions/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 ///////////////////////////////////////
 // Default parameters
 
@@ -28,3 +29,39 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+*/
+
+///////////////////////////////////////
+// How passing arguments works: value vs. reference
+
+const flight = 'LH234';
+const ania = {
+  name: 'Anna Muszyńska',
+  passport: 23482749362,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Ms. ' + passenger.name;
+
+  if (passenger.passport === 23482749362) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, ania);
+// console.log(flight);
+// console.log(ania);
+
+// // Is the same as doing...
+// const flightNum = flight;
+// const passenger = ania;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(ania);
+checkIn(flight, ania);
