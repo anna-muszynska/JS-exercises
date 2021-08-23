7e295ab880c1313e58969a30191ed638892ea96c
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-08-23 10:18:56 +0200

    Computing usernames

--- 09 Project #4 Bankist App/script.js
+++ 09 Project #4 Bankist App/script.js
@@ -80,3 +80,18 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+///////////////////////////////////////
+// Computing usernames
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
