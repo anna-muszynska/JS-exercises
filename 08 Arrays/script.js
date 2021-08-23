891b3afd952ef7a3822d823340dc494e9d88694d
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-08-23 11:21:52 +0200

    The filter method

--- 08 Arrays/script.js
+++ 08 Arrays/script.js
@@ -109,7 +109,7 @@ checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
 ///////////////////////////////////////
 // The map method
-
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const eurToUsd = 1.1;
@@ -135,3 +135,22 @@ const movementsDescriptions = movements.map(
 );
 
 console.log(movementsDescriptions);
+*/
+
+///////////////////////////////////////
+// The filter method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
