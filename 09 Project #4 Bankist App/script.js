748231e24d20c6612f41cc8783c591a713219625
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-08-24 22:26:00 +0200

    Calculate statistics (chaining methods)

--- 09 Project #4 Bankist App/script.js
+++ 09 Project #4 Bankist App/script.js
@@ -71,7 +71,7 @@ const displayMovements = function (movements) {
        <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}</div>
+        <div class="movements__value">${mov}€</div>
        </div>
     `;
 
@@ -101,6 +101,32 @@ createUsernames(accounts);
 
 const caclDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance} €`;
 };
 caclDisplayBalance(account1.movements);
+
+///////////////////////////////////////
+// Calculate statistics (chaining methods)
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
