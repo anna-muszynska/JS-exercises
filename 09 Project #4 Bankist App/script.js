9e7fff2b7721c19e61fa9700f17c84d99a603134
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-08-28 20:02:21 +0200

    Implementing the loan request: some and every

--- 09 Project #4 Bankist App/script.js
+++ 09 Project #4 Bankist App/script.js
@@ -196,6 +196,24 @@ btnTransfer.addEventListener('click', function (e) {
 });
 
 ///////////////////////////////////////
+// Implementing the loan request: some and every
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+///////////////////////////////////////
 // Closing the account (the findIndex method)
 
 btnClose.addEventListener('click', function (e) {
