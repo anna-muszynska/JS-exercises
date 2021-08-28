22139424dd7fdb8bf070ef0d018fafe7f0dccb8f
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-08-28 20:17:33 +0200

    Flat anf flatMap

--- 08 Arrays/script.js
+++ 08 Arrays/script.js
@@ -277,6 +277,7 @@ console.log(account);
 
 ///////////////////////////////////////
 // Some and every
+/*
 const account4 = {
   owner: 'Sarah Smith',
   movements: [430, 1000, 700, 50, 90],
@@ -305,3 +306,40 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+*/
+
+///////////////////////////////////////
+// Flat and flatMap
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+const account1 = {
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+};
+const account2 = {
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+};
+const account3 = {
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+};
+const account4 = {
+  movements: [430, 1000, 700, 50, 90],
+};
+const accounts = [account1, account2, account3, account4];
+
+// flat
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, cur) => acc + cur, 0);
+console.log(overalBalance);
+
+// flatMap
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, cur) => acc + cur, 0);
+console.log(overalBalance);
