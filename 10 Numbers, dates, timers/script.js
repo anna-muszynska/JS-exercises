02948aa0072ae65a919319ff63b0492897710b7d
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-08-29 22:06:26 +0200

    Converting and checking numbers

---
+++ 10 Numbers, dates, timers/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+///////////////////////////////////////
+// Converting and checking numbers
+
+// Base 10 - 0 to 9
+// Binary base - 0 1
+console.log(23 === 23.0);
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+// Conversion
+console.log(Number("23"));
+console.log(+"23");
+
+// Parsing
+console.log(Number.parseInt("30px", 10));
+console.log(Number.parseInt("e23", 10));
+
+console.log(Number.parseInt("2.5rem"));
+console.log(Number.parseFloat("2.5rem"));
+
+// Check is value in NaN
+console.log(Number.isNaN(20));
+console.log(Number.isNaN("20"));
+console.log(Number.isNaN(+"20K"));
+console.log(Number.isNaN(23 / 0));
+
+// Checking if value is number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite("20"));
+console.log(Number.isFinite(+"20K"));
+console.log(Number.isFinite(23 / 0));
+
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23 / 0));
