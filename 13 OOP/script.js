ae4e009326c882df1ff7875d2bf8ea37b0352f5f
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-09-06 23:13:38 +0200

    ES6 classes

--- 13 OOP/script.js
+++ 13 OOP/script.js
@@ -70,3 +70,67 @@ Array.prototype.unique = function () {
 };
 
 console.log(arr.unique());
+
+///////////////////////////////////////
+// Coding challenge #1
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+car1.accelerate();
+car1.accelerate();
+car2.brake();
+car2.brake();
+
+///////////////////////////////////////
+// ES6 Classes
+
+// class expression
+// const PersonCl = class {};
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const alicja = new PersonCl('Alicja', 1960);
+console.log(alicja);
+alicja.calcAge();
+
+console.log(alicja.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+alicja.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
