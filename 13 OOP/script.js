a4330171ba29b4a1800537f92c346fcd3a2e8b79
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-09-07 17:20:03 +0200

    Encapsulation: protected properties and methods

--- 13 OOP/script.js
+++ 13 OOP/script.js
@@ -436,32 +436,41 @@ jay.calcAge();
 ///////////////////////////////////////
 // Another class example
 
+///////////////////////////////////////
+// Encapsulation: protected properties and methods
+
 class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+
+    // Protected property
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
   // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
@@ -471,12 +480,13 @@ class Account {
 const acc1 = new Account('Ania', 'EUR', 1111);
 console.log(acc1);
 
-// acc1.movements.push(260);
-// acc1.movements.push(-140);
+// acc1._movements.push(260);
+// acc1._movements.push(-140);
 acc1.deposit(260);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
 acc1.approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
