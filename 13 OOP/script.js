abf8556a2561fe3a931181d8a850b6a996ee978f
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-09-07 18:40:39 +0200

    OOP exercise

--- 13 OOP/script.js
+++ 13 OOP/script.js
@@ -527,3 +527,65 @@ Account.helper();
 
 acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 console.log(acc1.getMovements());
+
+///////////////////////////////////////
+// Coding challenge #4
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(this.speed);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(this.speed);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+// console.log(rivian.#charge);
+
+rivian.accelerate().accelerate().brake().chargeBattery(90);
+console.log(rivian);
+
+console.log(rivian.speedUS);
