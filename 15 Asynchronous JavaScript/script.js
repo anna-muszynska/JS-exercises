1e5d0dcf1a10867a3ad17df5268e5cb41f02c3ed
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-09-10 14:59:31 +0200

    Promises and the fetch API - consuming promises

--- 15 Asynchronous JavaScript/script.js
+++ 15 Asynchronous JavaScript/script.js
@@ -48,7 +48,7 @@ getCountryData('portugal');
 
 ///////////////////////////////////////
 // Callback hell
-
+/*
 const renderCountry = function (data, className = '') {
   const html = `
       <article class="country ${className}">
@@ -122,3 +122,59 @@ setTimeout(() => {
     }, 1000);
   }, 1000);
 }, 1000);
+*/
+
+///////////////////////////////////////
+// Promises and the fetch API - consuming promises
+
+// const request = new XMLHttpRequest();
+// request.open('GET',`https://restcountries.eu/rest/v2/name/${country}?fullText=true`);
+// request.send();
+
+const renderCountry = function (data, className = '') {
+  const html = `
+        <article class="country ${className}">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  +data.population / 1000000
+                ).toFixed(1)} people</p>
+                <p class="country__row"><span>🗣️</span>${
+                  data.languages[0].name
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  data.currencies[0].name
+                }</p>
+            </div>
+        </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const request = fetch(
+  `https://restcountries.eu/rest/v2/name/netherlands?fullText=true`
+);
+console.log(request);
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('netherlands');
