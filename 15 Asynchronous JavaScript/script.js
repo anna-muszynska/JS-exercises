ffd99e6f5fa3b5dbafeacc589ba63a771494a909
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-09-10 16:38:16 +0200

    Chaining promises

--- 15 Asynchronous JavaScript/script.js
+++ 15 Asynchronous JavaScript/script.js
@@ -126,7 +126,7 @@ setTimeout(() => {
 
 ///////////////////////////////////////
 // Promises and the fetch API - consuming promises
-
+/*
 // const request = new XMLHttpRequest();
 // request.open('GET',`https://restcountries.eu/rest/v2/name/${country}?fullText=true`);
 // request.send();
@@ -170,11 +170,49 @@ console.log(request);
 //       renderCountry(data[0]);
 //     });
 // };
+*/
+
+///////////////////////////////////////
+// Chaining promises
+
+const renderCountry = function (data, className = '') {
+  const html = `
+          <article class="country ${className}">
+              <img class="country__img" src="${data.flag}" />
+              <div class="country__data">
+                  <h3 class="country__name">${data.name}</h3>
+                  <h4 class="country__region">${data.region}</h4>
+                  <p class="country__row"><span>👫</span>${(
+                    +data.population / 1000000
+                  ).toFixed(1)} people</p>
+                  <p class="country__row"><span>🗣️</span>${
+                    data.languages[0].name
+                  }</p>
+                  <p class="country__row"><span>💰</span>${
+                    data.currencies[0].name
+                  }</p>
+              </div>
+          </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
 const getCountryData = function (country) {
+  // Country 1
   fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
 getCountryData('netherlands');
