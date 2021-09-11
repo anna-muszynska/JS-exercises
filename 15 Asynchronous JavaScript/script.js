1b3d0203107441c9a1a2ea628d8d6fe9f89077f8
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-09-11 22:56:19 +0200

    The event loop

--- 15 Asynchronous JavaScript/script.js
+++ 15 Asynchronous JavaScript/script.js
@@ -177,7 +177,7 @@ console.log(request);
 
 ///////////////////////////////////////
 // Handling rejected promises
-
+/*
 const renderCountry = function (data, className = '') {
   const html = `
           <article class="country ${className}">
@@ -291,3 +291,74 @@ btn.addEventListener('click', function () {
 });
 
 getCountryData('australia');
+*/
+
+///////////////////////////////////////
+// Coding challenge #1
+/*
+const renderCountry = function (data, className = '') {
+  const html = `
+            <article class="country ${className}">
+                <img class="country__img" src="${data.flag}" />
+                <div class="country__data">
+                    <h3 class="country__name">${data.name}</h3>
+                    <h4 class="country__region">${data.region}</h4>
+                    <p class="country__row"><span>👫</span>${(
+                      +data.population / 1000000
+                    ).toFixed(1)} people</p>
+                    <p class="country__row"><span>🗣️</span>${
+                      data.languages[0].name
+                    }</p>
+                    <p class="country__row"><span>💰</span>${
+                      data.currencies[0].name
+                    }</p>
+                </div>
+            </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(response => {
+      console.log(response);
+
+      if (!response.ok)
+        throw new Error(
+          `To many requests per second - only 3 allowed ${response.status}`
+        );
+      return response.json();
+    })
+    .then(data => {
+      console.log(data, `You are in ${data.city}, ${data.country}`);
+      return fetch(
+        `https://restcountries.eu/rest/v2/name/${data.country}?fullText=true`
+      );
+    })
+    .then(response => {
+      if (!response.ok) throw new Error(`Country not found ${response.status}`);
+      return response.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.log(err.message));
+};
+
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
+*/
+
+///////////////////////////////////////
+// The event loop
+
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end  ');
